9b6a553df22258968fe167581735933f764c42ae
nick-cyber69 <nickdehadray@googlemail.com>
2025-08-24 12:57:08 +0000

    MAJOR UPDATE - New page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,11 @@
 export default function HomePage() {
   return (
-    <div style={{ padding: '2rem', textAlign: 'center' }}>
-      <h1 style={{ fontSize: '3rem', color: 'red' }}>TEST PAGE WORKING</h1>
-      <p style={{ fontSize: '1.5rem', color: 'blue' }}>
-        If you can see this, the page is updating correctly.
-      </p>
-      <div style={{
-        backgroundColor: 'yellow',
-        padding: '1rem',
-        margin: '1rem 0',
-        border: '2px solid green'
-      }}>
-        This should be a yellow box with green border
+    <div>
+      <h1>BRAND NEW PAGE - VERSION 2.0</h1>
+      <p>If you see this, the deployment is working!</p>
+      <div style={{ backgroundColor: 'red', color: 'white', padding: '20px' }}>
+        RED BOX TEST
       </div>
-      <p>Current time: {new Date().toLocaleString()}</p>
     </div>
   );
 }
-// Updated at Sun Aug 24 12:50:23 UTC 2025
